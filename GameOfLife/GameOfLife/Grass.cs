0f6b341d062b45b26b26cca73c9e48aeddbd86fe
GedeonKr <91838351+GedeonKr@users.noreply.github.com>
2023-10-20 17:56:31 +0200

    Update Grass.cs 3

--- GameOfLife/GameOfLife/Grass.cs
+++ GameOfLife/GameOfLife/Grass.cs
@@ -12,6 +12,7 @@ namespace GameOfLife
         public int posY { get; init; }
 
         //mező: maximum növés 2 (0 -> 1, 1 -> 2) (?)
+        
         private int size;
         public int Size
         {
@@ -22,7 +23,14 @@ namespace GameOfLife
                 else if (value > 2) size = 2;
                 else size = value;
             }
-    }
+        }
+        
+        public Grass(int posX, int posY)
+        {
+        Size = 0;
+        this.posX = posX;
+        this.posY = posY;
+        }
 
     }
 }
